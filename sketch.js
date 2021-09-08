54a47c5188a1a6f15f002520e9a0ee783018a9ba
bekkarysaicharanreddy <87811060+bekkarysaicharanreddy@users.noreply.github.com>
2021-09-08 15:07:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var weight=[34,54,67,67]
+var sum=weight[0]+weight[1]+weight[2]+weight[3]
+var average=sum/4
+console.log (average)
+function setup() 
+{
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
